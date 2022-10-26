53a9a43fb1c57b2af4d49c3cc8d6df3b4fb54dd5
Kamran <kamraneg@gmail.com>
2022-10-26 05:47:49 +0530

    ready to deploy for trials

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import Navbar from './components/Navbar'
 import Form from './components/Form'
 import { useState } from 'react';
 import Footer from './components/Footer';
+import Alert from './components/Alert';
 // import About from './components/About';
 
 function App() {
-  const [mode, setMode] = useState("light")
+  const [alert, setAlert] = useState(null)  //initial alert set to null
+ let showAlert = (message, type) =>{        // parameters for alets
+      setAlert({
+      msg: message,                          //object created
+      type: type
+     })
+     setTimeout(() => {
+      setAlert(null)                         //async timeout for alert display, time mentioned in milliseconds
+     }, 1800);
+ }
+
+  const [mode, setMode] = useState("light")     
   let toggleMode = ()=>{
     if(mode === "light"){
         setMode("dark")
-        document.body.style.backgroundColor = '#0b1239';
+        document.body.style.backgroundColor = '#0b1239';        
+        showAlert("Dark mode enabled","success")
     }else{
       setMode("light")
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled","success")
     }
   }
   return (
     <>
       <Navbar title="Text-Utils" mode= {mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <Form heading="Convert your text :" mode={mode}/>
-      <Footer mode= {mode}/>
+      <Footer mode= {mode}/>    
       {/* <About/> */}
     </>
   );
---
+++ src/components/Alert.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Alert(props) {
+        let capital =(str)=>{                           //function for making string first letter capital & return 
+            let low =str.toLowerCase()
+            return low.charAt(0).toUpperCase() + low.slice(1) 
+        }
+  return (
+    props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <strong>{capital(props.alert.type)}: </strong>{props.alert.msg}                                             {/*message popup */}
+    {/* <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+    </div>
+ );
+}
